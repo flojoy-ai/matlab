8938cf5761e3f33e8bf2870cb38e9af59cede4e0
Tristan Britt <tristan.britt@mail.mcgill.ca>
2023-08-19 10:41:45 -0400

    added basic error handling on api calls

--- flojoy/FlojoyCloud.m
+++ flojoy/FlojoyCloud.m
@@ -3,6 +3,45 @@ classdef FlojoyCloud
         api_key {mustBeNonzeroLengthText} = '0'
     end
 
+
+    methods (Access = private)
+        function verify_responsecode(httpCode)
+            switch httpCode
+                case 200
+                    return;
+                case 400
+                    warning('HTTP Bad Request (400)');
+                case 401
+                    warning('HTTP Unauthorized (401)');
+                case 403
+                    warning('HTTP Forbidden (403)');
+                case 404
+                    warning('HTTP Not Found (404)');
+                case 500
+                    warning('HTTP Internal Server Error (500)');
+                otherwise
+                    warning(['HTTP Response Code: ', num2str(httpCode)]);
+            end
+        end
+
+        function response = verified_webread(uri, options)
+            try
+                response = webread(uri, options);
+                verify_responsecode(response.StatusCode);
+            catch ME
+                error(['Error reading from the Flojoy API: ', ME.message]);
+            end
+        end
+        
+        function response = verified_webwrite(uri, payload, options)
+            try
+                response = webwrite(uri, payload, options);
+                verify_responsecode(response.StatusCode);
+            catch ME
+                error(['Error writing to the Flojoy API: ', ME.message]);
+            end
+        end
+
     methods
         function r = fetch_dc(obj, dc_id)
             import webread.*
@@ -12,7 +51,7 @@ classdef FlojoyCloud
             urispec = "https://cloud.flojoy.ai/api/v1/dcs/%s";
             uri = sprintf(urispec,dc_id);
             uri = matlab.net.URI(uri);
-            response = webread(uri, options);
+            response = verified_webread(uri, options);
 
             message = "Returning DataContainer of type: %s\n";
             fprintf(message,response.dataContainer.type);
@@ -54,7 +93,7 @@ classdef FlojoyCloud
 
             payload.data.type = dc_type;
             payload = jsonencode(payload);
-            response = webwrite(uri, payload, options);
+            response = verified_webwrite(uri, payload, options);
             message = "Successfully sent DataContainer with ID: %s\n";
             fprintf(message,response.dc_id);
             r = response.dc_id;
