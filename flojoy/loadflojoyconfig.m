f6279aa9f36aecb9123688390b0984d10ff61513
Tristan Britt <tristan.britt@mail.mcgill.ca>
2023-08-19 10:45:39 -0400

    easier call to get user homedir

--- flojoy/loadflojoyconfig.m
+++ flojoy/loadflojoyconfig.m
@@ -1,20 +1,23 @@
 function api_key = loadflojoyconfig()
-% Save flojoy config info.
-% in ~/.flojoy/.config
+    % Save flojoy config info.
+    % in ~/.flojoy/.config
 
-% Create the .flojoy folder
-if ispc
-    %     userDir = winqueryreg('HKEY_CURRENT_USER',...
-    %         ['Software\Microsoft\Windows\CurrentVersion\' ...
-    %          'Explorer\Shell Folders'],'Personal');
-    userhome = getenv('appdata');
-else
-    userhome = char(java.lang.System.getProperty('user.home'));
-end
+    % Create the .flojoy folder
+    if ispc
+        %     userDir = winqueryreg('HKEY_CURRENT_USER',...
+        %         ['Software\Microsoft\Windows\CurrentVersion\' ...
+        %          'Explorer\Shell Folders'],'Personal');
+        userhome = getenv('appdata');
+    else
+        % userhome = char(java.lang.System.getProperty('user.home'));
+        userhome = userpath; % this is a system defined variable for the users home dir
+                             % loading the whole java lib is bloating
+        [userhome, ~, ~] = fileparts(userhome);
+    end
 
-flojoy_config_file = "%s/.flojoy/cloud_key.txt";
-flojoy_config_file = sprintf(flojoy_config_file,userhome);
+    flojoy_config_file = "%s/.flojoy/cloud_key.txt";
+    flojoy_config_file = sprintf(flojoy_config_file,userhome);
 
-api_key = char(importdata(flojoy_config_file));
+    api_key = char(importdata(flojoy_config_file));
 
 end
