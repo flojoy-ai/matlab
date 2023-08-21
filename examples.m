44da04f5d1d76a2047813b2e8dac238ad4546744
dstrande <dstrande@live.ca>
2023-08-21 15:40:39 -0700

    Matlab API now in line with Python

--- examples.m
+++ examples.m
@@ -1,19 +1,58 @@
+addpath flojoy
+
 import webread.*
 import matlab.net.*
 import flojoy.*
 
+% To save your api key paste it in place of <key example> and uncomment 
+% the next line. After the key is saved loadflojoyconfig will load the key.
+
 % saveflojoyconfig('key_example')
 
-data = [12 14; 5 12];
-dc_type = "Matrix";
-
-
-% FlojoyCloud.api_key = loadflojoyconfig;
-% 
+% Initialize the API and load the api key.
 cloud = FlojoyCloud;
 cloud.api_key = loadflojoyconfig;
 
-dc_id = cloud.store_dc(data, dc_type);
+% Uncomment the example sections you want to run. 
 
-cloud.fetch_dc(dc_id)
+%% 
+
+% % Creating, storing and loading example dataset.
+% 
+% % Simple example dataset to save.
+% data = [12 14; 5 12];
+% dc_type = "Matrix";
+% 
+% % Store the example dataset to the cloud.
+% dc_id = cloud.store_dc(data, dc_type)
+% 
+% % Load the example dataset from the cloud.
+% cloud.fetch_dc(dc_id)
+
+%% 
+
+% % Using a measurement folder.
+% 
+% % Simple example dataset to save.
+% data = [12 14; 5 12];
+% dc_type = "Matrix";
+% 
+% % Create a measurement folder in the cloud with the name specified.
+% meas_id = cloud.create_measurement('default');
+% 
+% % Store a datacontainer in the created measurement folder.
+% cloud.store_in_measurement(data, dc_type, meas_id);
+% 
+% % Fetch a measurement folder in the cloud with the id specified.
+% measurement = cloud.fetch_measurement(meas_id);
+
+%% 
+
+% % Listing multiple measurements and DataContainers
+% 
+% % List 10 (unless there's less) DataContainers stored in the cloud.
+% dc_list = cloud.list_dcs(10);
+% 
+% % List the number of measurements specified.
+% meas_list = cloud.list_measurement(10);
 
--- flojoy/FlojoyCloud.m
+++ flojoy/FlojoyCloud.m
@@ -1,52 +1,13 @@
 classdef FlojoyCloud
     properties
-        api_key {mustBeNonzeroLengthText} = '0'
+        api_key {mustBeNonzeroLengthText} = '0';
     end
-
-
-    methods (Access = private)
-        function verify_responsecode(httpCode)
-            switch httpCode
-                case 200
-                    return;
-                case 400
-                    warning('HTTP Bad Request (400)');
-                case 401
-                    warning('HTTP Unauthorized (401)');
-                case 403
-                    warning('HTTP Forbidden (403)');
-                case 404
-                    warning('HTTP Not Found (404)');
-                case 500
-                    warning('HTTP Internal Server Error (500)');
-                otherwise
-                    warning(['HTTP Response Code: ', num2str(httpCode)]);
-            end
-        end
-
-        function response = verified_webread(uri, options)
-            try
-                response = webread(uri, options);
-                verify_responsecode(response.StatusCode);
-            catch ME
-                error(['Error reading from the Flojoy API: ', ME.message]);
-            end
-        end
-        
-        function response = verified_webwrite(uri, payload, options)
-            try
-                response = webwrite(uri, payload, options);
-                verify_responsecode(response.StatusCode);
-            catch ME
-                error(['Error writing to the Flojoy API: ', ME.message]);
-            end
-        end
 
     methods
         function r = fetch_dc(obj, dc_id)
             import webread.*
             import matlab.net.*
-            headers = {'api_key' obj.api_key; 'Content-Type' 'application/json'};
+            headers = {'api_key' obj.api_key};
             options = weboptions('HeaderFields', headers, RequestMethod='GET');
             urispec = "https://cloud.flojoy.ai/api/v1/dcs/%s";
             uri = sprintf(urispec,dc_id);
@@ -58,14 +19,7 @@ classdef FlojoyCloud
             r = response.dataContainer;
         end
 
-        function r = store_dc(obj, dc, dc_type)
-            import webread.*
-            import matlab.net.*
-            headers = {'api_key' obj.api_key; 'Content-Type' 'application/json'};
-            options = weboptions('HeaderFields', headers, RequestMethod='POST');
-            uri = "https://cloud.flojoy.ai/api/v1/dcs";
-            uri = matlab.net.URI(uri);
-            
+        function r = create_payload(obj, dc, dc_type)
             switch dc_type
                 case {'Matrix','DataFrame','Grayscale'}
                     payload = struct();
@@ -89,14 +43,105 @@ classdef FlojoyCloud
                     assert(size(dc) == 1,'DataContainer is not a single number (float/integer)')
                     payload = struct();
                     payload.data.c = double(dc);
+                case 'Vector'
+                    payload = struct();
+                    payload.data.v = dc;
+                otherwise
+                    error('Unsupported DataContainer type. Check case (e.g. OrderedPair).')
             end
-
             payload.data.type = dc_type;
-            payload = jsonencode(payload);
-            response = verified_webwrite(uri, payload, options);
+            r = jsonencode(payload);
+        end
+
+        function r = store_dc(obj, dc, dc_type)
+            import webread.*
+            import matlab.net.*
+            headers = {'api_key' obj.api_key};
+            options = weboptions('HeaderFields', headers, RequestMethod='POST');
+            uri = "https://cloud.flojoy.ai/api/v1/dcs";
+            uri = matlab.net.URI(uri);
+            payload = obj.create_payload(dc, dc_type);
+            response = webwrite(uri, payload, options);
             message = "Successfully sent DataContainer with ID: %s\n";
             fprintf(message,response.dc_id);
             r = response.dc_id;
+        end
+
+        function r = list_dcs(obj, size)
+            import webread.*
+            import matlab.net.*
+            headers = {'api_key' obj.api_key};
+            options = weboptions('HeaderFields', headers, RequestMethod='GET');
+            uri = "https://cloud.flojoy.ai/api/v1/dcs";
+            uri = matlab.net.URI(uri);
+
+            try
+                response = webread(uri, 'size', size, 'inbox', 'true', options);
+                r = response.data;
+            catch ME
+                switch ME.identifier
+                    case 'MATLAB:webservices:HTTP500StatusCodeError'
+                        warning('HTTP Error 500: Reducing the number of DataContainers to list may fix this issue.')
+                    otherwise
+                        rethrow(ME)
+                end
+            end
+        end
+
+        function r = create_measurement(obj, name, privacy)
+            if nargin < 3
+                privacy = 'private';
+            end
+            import webread.*
+            import matlab.net.*
+            headers = {'api_key' obj.api_key};
+            options = weboptions('HeaderFields', headers, RequestMethod='POST');
+            uri = "https://cloud.flojoy.ai/api/v1/measurements";
+            uri = matlab.net.URI(uri);
+            payload = struct();
+            payload.name = name;
+            payload.privacy = privacy;
+            payload = jsonencode(payload);
+            response = webwrite(uri, payload, options);
+            r = response.ref;
+        end
+
+        function r = list_measurement(obj, size)
+            import webread.*
+            import matlab.net.*
+            headers = {'api_key' obj.api_key};
+            options = weboptions('HeaderFields', headers, RequestMethod='GET');
+            uri = "https://cloud.flojoy.ai/api/v1/measurements";
+            uri = matlab.net.URI(uri);
+            response = webread(uri, 'size', size, options);
+            r = response.data;
+        end
+
+        function r = fetch_measurement(obj, meas_id)
+            import webread.*
+            import matlab.net.*
+            headers = {'api_key' obj.api_key};
+            options = weboptions('HeaderFields', headers, RequestMethod='GET');
+            urispec = "https://cloud.flojoy.ai/api/v1/measurements/%s";
+            uri = sprintf(urispec,meas_id);
+            uri = matlab.net.URI(uri);
+            response = webread(uri, options);
+            r = response;
+        end
+
+        function r = store_in_measurement(obj, dc, dc_type, meas_id)
+            import webread.*
+            import matlab.net.*
+            headers = {'api_key' obj.api_key};
+            options = weboptions('HeaderFields', headers, RequestMethod='POST');
+            urispec = "https://cloud.flojoy.ai/api/v1/measurements/%s";
+            uri = sprintf(urispec,meas_id);
+            uri = matlab.net.URI(uri);
+            payload = obj.create_payload(dc, dc_type);
+            response = webwrite(uri, payload, options);
+            % message = "Successfully sent DataContainer with ID: %s\n";
+            % fprintf(message,response);
+            r = response;
         end
     end
 end
