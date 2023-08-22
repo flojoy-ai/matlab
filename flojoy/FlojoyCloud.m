9be3f789b6a8ae3d8164509d50809f56ef620f20
dstrande <dstrande@live.ca>
2023-08-22 15:07:06 -0700

    Matlab API alpha v001

--- flojoy/FlojoyCloud.m
+++ flojoy/FlojoyCloud.m
@@ -5,6 +5,7 @@ classdef FlojoyCloud
 
     methods
         function r = fetch_dc(obj, dc_id)
+            % Fetch a datacontainer from the cloud.
             import webread.*
             import matlab.net.*
             headers = {'api_key' obj.api_key};
@@ -12,7 +13,7 @@ classdef FlojoyCloud
             urispec = "https://cloud.flojoy.ai/api/v1/dcs/%s";
             uri = sprintf(urispec,dc_id);
             uri = matlab.net.URI(uri);
-            response = verified_webread(uri, options);
+            response = webread(uri, options);
 
             message = "Returning DataContainer of type: %s\n";
             fprintf(message,response.dataContainer.type);
@@ -20,44 +21,98 @@ classdef FlojoyCloud
         end
 
         function r = create_payload(obj, dc, dc_type)
+            % Ready a DataContainer for storage from matlab data.
+            payload = struct();
+            payload.data.type = dc_type;
             switch dc_type
                 case {'Matrix','DataFrame','Grayscale'}
-                    payload = struct();
                     payload.data.m = dc;
                 case 'OrderedPair'
-                    payload = struct();
+                    assert(isa(dc, "struct"), 'DataContainer should be class "structure"')
+                    assert(isfield(dc, 'x'), 'Field "x" missing from DataContainer')
+                    assert(isfield(dc, 'y'), 'Field "y" missing from DataContainer')
                     payload.data.x = dc.x;
                     payload.data.y = dc.y;
                 case 'OrderedTriple'
-                    payload = struct();
+                    assert(isa(dc, "struct"), 'DataContainer should be class "structure"')
+                    assert(isfield(dc, 'x'), 'Field "x" missing from DataContainer')
+                    assert(isfield(dc, 'y'), 'Field "y" missing from DataContainer')
+                    assert(isfield(dc, 'z'), 'Field "z" missing from DataContainer')
                     payload.data.x = dc.x;
                     payload.data.y = dc.y;
                     payload.data.z = dc.z;
                 case 'Image'
-                    payload = struct();
                     payload.data.r = dc(:,:,1); % Red channel;
-                    payload.data.g = dc(:,:,2); % Red channel;
-                    payload.data.b = dc(:,:,3); % Red channel;
+                    payload.data.g = dc(:,:,2); % Green channel;
+                    payload.data.b = dc(:,:,3); % Blue channel;
                 case 'Scalar'
-                    dc = double(dc);
-                    assert(size(dc) == 1,'DataContainer is not a single number (float/integer)')
-                    payload = struct();
-                    payload.data.c = double(dc);
+                    if isa(dc,'double')
+                        assert(size(dc,1) == 1,'DataContainer is not a single number (float/integer)')
+                        assert(size(dc,2) == 1,'DataContainer is not a single number (float/integer)')
+                        dc = double(dc);
+                    elseif isa(dc,'struct')
+                        dc = double(dc.c);
+                        assert(size(dc,1) == 1,'DataContainer is not a single number (float/integer)')
+                        assert(size(dc,2) == 1,'DataContainer is not a single number (float/integer)')
+                    end
+                    payload.data.c = dc;
                 case 'Vector'
-                    payload = struct();
                     payload.data.v = dc;
                 otherwise
                     error('Unsupported DataContainer type. Check case (e.g. OrderedPair).')
             end
-            payload.data.type = dc_type;
             r = jsonencode(payload);
         end
 
+        function r = to_matlab(obj, dc)
+            % Deserialize data into matlab types
+            dc_type = dc.type;
+            switch dc_type
+                case {'Matrix','Grayscale'}
+                    assert(isfield(dc, 'm'), 'Field "m" missing from DataContainer')
+                    dc = dc.m;
+                    assert(isa(dc,'double'),'DC is not type double.')
+                case 'DataFrame'
+                    assert(isfield(dc, m), 'Field "m" missing from DataContainer')
+                    dc = dc.m;
+                case 'OrderedPair'
+                    assert(isfield(dc, 'x'), 'Field "x" missing from DataContainer')
+                    assert(isfield(dc, 'y'), 'Field "y" missing from DataContainer')
+                    data.x = dc.x;
+                    data.y = dc.y;
+                    dc = data;
+                case 'OrderedTriple'
+                    assert(isfield(dc, 'x'), 'Field "x" missing from DataContainer')
+                    assert(isfield(dc, 'y'), 'Field "y" missing from DataContainer')
+                    assert(isfield(dc, 'z'), 'Field "y" missing from DataContainer')
+                    data.x = dc.x;
+                    data.y = dc.y;
+                    data.z = dc.z;
+                    dc = data;
+                case 'Image'
+                    assert(isfield(dc, 'r'), 'Field "r" missing from DataContainer')
+                    assert(isfield(dc, 'g'), 'Field "g" missing from DataContainer')
+                    assert(isfield(dc, 'b'), 'Field "b" missing from DataContainer')
+                    dc = uint8(cat(3,dc.r,dc.g,dc.b));
+                case 'Scalar'
+                    dc = double(dc.c);
+                    assert(size(dc,1) == 1,'DataContainer is not a single number (float/integer)')
+                    assert(size(dc,2) == 1,'DataContainer is not a single number (float/integer)')
+                case 'Vector'
+                    dc = dc.v;
+                    assert(size(dc,2) == 1,'DataContainer is not 1-dimensional (vector)')
+                otherwise
+                    error('Unsupported DataContainer type. Check case (e.g. OrderedPair).')
+            end
+            r = dc;
+        end
+
         function r = store_dc(obj, dc, dc_type)
+            % Store a DataContainer in the cloud.
             import webread.*
             import matlab.net.*
             headers = {'api_key' obj.api_key};
-            options = weboptions('HeaderFields', headers, RequestMethod='POST');
+            options = weboptions('HeaderFields', headers, 'Timeout', 30, RequestMethod='POST');
             uri = "https://cloud.flojoy.ai/api/v1/dcs";
             uri = matlab.net.URI(uri);
             payload = obj.create_payload(dc, dc_type);
@@ -68,6 +123,8 @@ classdef FlojoyCloud
         end
 
         function r = list_dcs(obj, size)
+            % List the number of DataContainers specified.
+            % Trying to list more DataContainers than exist will cause an error.
             import webread.*
             import matlab.net.*
             headers = {'api_key' obj.api_key};
@@ -89,6 +146,8 @@ classdef FlojoyCloud
         end
 
         function r = create_measurement(obj, name, privacy)
+            % Create a measurement 'folder' to store DataContainers in.
+            % Measurement privacy can be set to private or public.
             if nargin < 3
                 privacy = 'private';
             end
@@ -107,6 +166,7 @@ classdef FlojoyCloud
         end
 
         function r = list_measurement(obj, size)
+            % List the number of measurements specified.
             import webread.*
             import matlab.net.*
             headers = {'api_key' obj.api_key};
@@ -118,6 +178,7 @@ classdef FlojoyCloud
         end
 
         function r = fetch_measurement(obj, meas_id)
+            % Fetch the measurement with the ID specified.
             import webread.*
             import matlab.net.*
             headers = {'api_key' obj.api_key};
@@ -130,6 +191,7 @@ classdef FlojoyCloud
         end
 
         function r = store_in_measurement(obj, dc, dc_type, meas_id)
+            % Store a DataContainer in the measurement specified.
             import webread.*
             import matlab.net.*
             headers = {'api_key' obj.api_key};
@@ -139,8 +201,6 @@ classdef FlojoyCloud
             uri = matlab.net.URI(uri);
             payload = obj.create_payload(dc, dc_type);
             response = webwrite(uri, payload, options);
-            % message = "Successfully sent DataContainer with ID: %s\n";
-            % fprintf(message,response);
             r = response;
         end
     end
